2f5161025596e365c0fff44a032b418cbbc89824
caiofct <caiofct@gmail.com>
2016-05-29 14:59:53 -0300

    Added unit tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
 
+  it "is invalid without a name" do
+    expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
+  end
+
+  it "is invalid without an email" do
+    expect(FactoryGirl.build(:user, email: nil)).not_to be_valid
+  end
+
+  it "is invalid without a username" do
+    expect(FactoryGirl.build(:user, username: nil)).not_to be_valid
+  end
+
+  it "is invalid without a password" do
+    expect(FactoryGirl.build(:user, password: nil)).not_to be_valid
+  end
+
+  it "is invalid with the username as an email address" do
+    expect(FactoryGirl.build(:user, username: Faker::Internet.email)).not_to be_valid
+  end
+
+  it "does not allow users with duplicated emails" do
+    duplicated_email = Faker::Internet.email
+    FactoryGirl.create(:user, email: duplicated_email)
+    expect(FactoryGirl.build(:user, email: duplicated_email)).not_to be_valid
+  end
+
+  it "does not allow users with duplicated usernames" do
+    duplicated_username = Faker::Internet.user_name
+    FactoryGirl.create(:user, username: duplicated_username)
+    expect(FactoryGirl.build(:user, username: duplicated_username)).not_to be_valid
+  end
 end
