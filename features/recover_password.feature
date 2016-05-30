cce7baf8aae4c288e7822d42058c168076fdbb3d
caiofct <caiofct@gmail.com>
2016-05-29 21:21:14 -0300

    Adding recover password acceptance tests

---
+++ features/recover_password.feature
@@ -0,0 +1,18 @@
+Feature: Recover password for an existing account
+  As an user
+  When I forget my password
+  I want to be able to recover it
+
+  Scenario: Must fill the recover password form and be able to generate a new password
+    Given that I am on the recover_password page
+    When I fill in the email field
+    And click in "Enviar"
+    Then I should be redirected to the sign_in page
+    And I should see the message "Dentro de minutos, você receberá um e-mail com instruções para a troca da sua senha."
+    And I should receive a recover password email
+    And I should be able to click in the recover password link on the email to got to the change password page
+    And I should fill the recover password form with my new password
+    And click in "Alterar senha"
+    And I should be redirected to the homepage
+    And I should see the message "Sua senha foi alterada com sucesso. Você está logado."
+    And the new password was really changed
---
+++ features/step_definitions/recover_password_steps.rb
@@ -0,0 +1,34 @@
+Given(/^that I am on the recover_password page$/) do
+  visit new_user_password_path
+end
+
+When(/^I fill in the email field$/) do
+  @user = FactoryGirl.build(:user)
+  @user.skip_confirmation!
+  @user.save
+
+  fill_in "user_email", with: @user.email
+end
+
+Then(/^I should be redirected to the sign_in page$/) do
+  expect(current_path).to eq new_user_session_path
+end
+
+Then(/^I should receive a recover password email$/) do
+  expect(last_email.body.match("Esqueceu a senha")).not_to be_blank
+end
+
+Then(/^I should be able to click in the recover password link on the email to got to the change password page$/) do
+  @reset_password_token = last_email.body.match(/reset_password_token=([^"]+)/)
+  visit "/users/password/edit?#{@reset_password_token[0]}"
+end
+
+Then(/^I should fill the recover password form with my new password$/) do
+  @new_password = Faker::Internet.password(8, 20, true, true)
+  fill_in "user_password", with: @new_password
+  fill_in "user_password_confirmation", with: @new_password
+end
+
+Then(/^the new password was really changed$/) do
+  expect(User.find_by_email(@user.email).valid_password?(@new_password)).to be true
+end
