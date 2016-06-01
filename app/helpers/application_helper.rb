b5dc1aad35f2deb08739a84068caf12d86ef990f
caiofct <caiofct@gmail.com>
2016-05-31 22:17:52 -0300

    Finished the acceptance tests for the create post feature and for the view posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # Displays the user profile image using a variety of differents sizes an css options
   def profile_image(size = :default, options = {}, user = current_user)
 
-    profile_style = ""
+    profile_style = "width: 100px; height: 100px;"
     profile_class = "profile-image img-circle circle-border m-b-md"
     case size.to_sym
     when :very_small
@@ -11,8 +11,6 @@ module ApplicationHelper
     when :small
       profile_style = "width: 60px; height: 60px;"
     when :default
-      profile_style = "width: 100px; height: 100px;"
-    else
       profile_style = "width: 100px; height: 100px;"
     end
 
--- features/step_definitions/create_posts_steps.rb
+++ features/step_definitions/create_posts_steps.rb
@@ -3,9 +3,18 @@ Given(/^I am on the posts page$/) do
 end
 
 When(/^I fill in the post content$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @post_content = Faker::Lorem.paragraph
+  @posts_count = page.all(:css, ".posts div.post").length
+  fill_in "post_content", with: @post_content
 end
 
 Then(/^I should see my new post on the top of the list of existing posts$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_content?(@post_content)
+  # The size of the posts list should increment by 1
+  expect(page.all(:css, ".posts div.post").length).to eq @posts_count + 1
+end
+
+Then(/^I shouldn't see any post added to the list of existing posts$/) do
+  # The size of the posts list shouldn't change
+  expect(page.all(:css, ".posts div.post").length).to eq 0
 end
