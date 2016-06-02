b3313d8795fb8a6062612147769a75293d0172c3
Caio Teixeira <caiofct@ifce.edu.br>
2016-06-02 16:33:00 -0300

    Added funcionatilty to follow/unfollow a user and to view the posts from the followed users in the users home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   # An user has many posts
   has_many :posts, dependent: :destroy
   # Mapping the relationships for Followers and Followeds
+  has_many :followings
   has_many :followerships, class_name: 'Following', foreign_key: 'user_id'
   has_many :followedships, class_name: 'Following', foreign_key: 'follower_id'
   has_many :followers, through: :followerships, source: :follower
@@ -28,9 +29,40 @@ class User < ApplicationRecord
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
 
   # Finds an specific user by username
-  scope :by_username, lambda { |username|  where("username" => username)}
+  scope :by_username, lambda { |username| where("username" => username) }
   # Finds all confirmed users
   scope :confirmed, lambda { where("confirmed_at IS NOT NULL") }
+
+  # To determine whether a user can follow another, we have some pre-requisites:
+  # 1. The user to be followed can`t be the same user that wants to follow him (A user cannot follow himself)
+  # 2. The user to be followed can`t be followed yet by the user (You cannot have duplicates between a follower and followed pair)
+  def can_follow?(user)
+    if self.id == user.id || following?(user)
+      return false
+    end
+    true
+  end
+
+  # Verifying whether a user is following another one
+  def following?(user)
+    followed = Following.find_by_follower_id_and_user_id(self.id, user.id)
+    return false if followed.blank?
+    true
+  end
+
+  # Adds the user in parameter to the list of followeds of the instance user
+  def follow(user)
+    self.followeds << user
+  end
+
+  # Removes the user in the parameter from the Followings table where the
+  # instance user is in the follower_id and the parameter user is in the user_id
+  def unfollow(user)
+    followed = Following.find_by_follower_id_and_user_id(self.id, user.id)
+    if !followed.blank?
+      followed.destroy
+    end
+  end
 
   # Overrinding the devise's logic to lookup a user in the database to use
   # the username or email
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :users, only: [:update]
+
   get '/user/:username' => 'users#show', as: :user_profile, constraints: { username: /[^\/]+/ }
+  get "/user/:username/follow" => 'users#follow', as: :follow_user, constraints: { username: /[^\/]+/ }
+  get "/user/:username/unfollow" => 'users#unfollow', as: :unfollow_user, constraints: { username: /[^\/]+/ }
 
   root 'home#index'
 end
