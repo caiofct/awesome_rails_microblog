75bd0f2099a2cc6b7e066347dc99d555dc1aedc0
Caio Teixeira <caiofct@ifce.edu.br>
2016-08-09 16:32:04 -0300

    Adding redis and action_cable configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.web_socket_server_url = "wss://awesome-rails-microblog.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://awesome-rails-microblog.herokuapp.com', 'http://awesome-rails-microblog.herokuapp.com']
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
